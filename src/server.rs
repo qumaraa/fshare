b6b369336f395fcd12a0190b523535395cfe1949
Alexander <qumara99@icloud.com>
2024-04-03 19:11:34 +0400

    `upd`: 1.0

--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,12 @@
-use std::path::PathBuf;
-use actix_web::{App, CustomizeResponder, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::{web, App, CustomizeResponder, HttpRequest, HttpResponse, HttpServer, Responder};
 use rand::Rng;
-#[derive(Clone,Debug)]
+use std::path::PathBuf;
+use crate::qr::show_qr;
+
+#[derive(Clone, Debug)]
 pub enum Action {
     Download { file_path: PathBuf },
 }
-
-pub async fn start(action: Action) -> std::io::Result<()> {
-    let mut rng = rand::thread_rng();
-    let port = rng.gen_range(49152..=65535);
-    let action_clone = web::Data::new(action.clone());
-    let srv = HttpServer::new({
-        move || {
-            let mut app = App::new()
-                .app_data(action_clone.clone());
-            match action.clone() {
-                Action::Download { file_path: _ } => {
-                    app = app.service(web::resource("/download").route(web::get().to(download)))
-                }
-            } app
-        }
-    }).bind(("0.0.0.0",port.clone()))
-        .unwrap()
-        .run();
-    println!("[*] Server is listening on 0.0.0.0:{}", port);
-    srv.await
-}
-
 
 pub async fn download(
     req: HttpRequest,
@@ -36,7 +16,7 @@ pub async fn download(
         Action::Download { file_path } => file_path.clone(),
         _ => {
             panic!("file_path not found.");
-        },
+        }
     };
     let file_name = file_path.file_name().unwrap().to_str().unwrap();
     println!("Sending file {:?}...", file_path);
@@ -44,9 +24,32 @@ pub async fn download(
         .await
         .unwrap();
 
-
     file.into_response(&req).customize().insert_header((
         "Content-Disposition",
         format!("attachment; filename=\"{}\"", file_name),
     ))
+}
+
+pub async fn start(action: Action) -> std::io::Result<()> {
+    let mut rng = rand::thread_rng();
+    let port = rng.gen_range(49152..=65535);
+    let action_clone = web::Data::new(action.clone());
+    let action_clone2 = action.clone();
+    let srv = HttpServer::new({
+        move || {
+            let mut app = App::new().app_data(action_clone.clone());
+            match action.clone() {
+                Action::Download { file_path: _ } => {
+                    app = app.service(web::resource("/download").route(web::get().to(download)))
+                }
+            }
+            app
+        }
+    })
+    .bind(("0.0.0.0", port.clone()))
+    .unwrap()
+    .run();
+    show_qr(action_clone2, port);
+    println!("[*] Server is listening on 0.0.0.0:{}", port);
+    srv.await
 }
