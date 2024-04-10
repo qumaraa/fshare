538918df7878cd006458438f8f46b6491dc10601
Alexander <112755279+qumaraa@users.noreply.github.com>
2024-04-10 10:31:27 +0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod cli;
 mod qr;
 mod recv;
 mod server;
-//mod recv;
 
 static VERSION: &'static str = "2.0";
 
-#[actix_web::main]
+#[actix_web::main] // actix-web runtime
 async fn main() {
+    // starting cli
     let _ = cli::start().await;
 }
